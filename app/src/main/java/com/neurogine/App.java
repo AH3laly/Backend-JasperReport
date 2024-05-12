c82aa0a5384f2c832708315b88ceb80a2c90eaad
Ah3laly <Ah3laly@gmail.com>
2024-05-12 14:47:57 +0800

    Implement demo dataSource

--- app/src/main/java/com/neurogine/App.java
+++ app/src/main/java/com/neurogine/App.java
@@ -1,26 +1,31 @@
 package com.neurogine;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import com.neurogine.beans.PdfReportRow;
 import com.neurogine.models.ReportDocument;
 import com.neurogine.models.ReportDocumentField;
 import com.neurogine.services.JapserReportServiceImpl;
 
 import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 public class App {
 	
     public static void main(String[] args) {
     	try {
     		
+    		// Create ReportDocument
     		ReportDocument reportDocument = new ReportDocument();
         	reportDocument.setName("First Demo Report");
         	reportDocument.setHeaderFields(generateDemofields());
         	
+        	// Pass ReportDocument to JapserReportService
         	JapserReportServiceImpl jasperReportService = new JapserReportServiceImpl();
         	jasperReportService.createNewReport(reportDocument, createDemoDataSource());
         	
-        	// Export jrxml file
+        	// Export JRXML file
         	String jrxmlDestFileFile = "/projects/results/Output.jrxml";
         	jasperReportService.generateJrxml(jrxmlDestFileFile);
         	
@@ -34,13 +39,36 @@ public class App {
     	}
     }
     
-    private static ArrayList<ReportDocumentField> generateDemofields(){
-    	return new ArrayList<ReportDocumentField>();
+    private static List<ReportDocumentField> generateDemofields(){
+    	
+    	// date, referenceno, amount, status, remark
+    	List<ReportDocumentField> fieldsList = new ArrayList<>();
+    	fieldsList.add(new ReportDocumentField("date", "java.lang.String", "Date"));
+    	fieldsList.add(new ReportDocumentField("referenceno", "java.lang.String", "Reference No"));
+    	fieldsList.add(new ReportDocumentField("amount", "java.lang.String", "Amount"));
+    	fieldsList.add(new ReportDocumentField("status", "java.lang.String", "Status"));
+    	fieldsList.add(new ReportDocumentField("remark", "java.lang.String", "Remark"));
+    	
+    	return fieldsList;
     }
 
     private static JRDataSource createDemoDataSource() throws InstantiationException, IllegalAccessException {
-    	return JRDataSource.class.newInstance();
-		
+
+    	List<PdfReportRow> tableData = new ArrayList<PdfReportRow>();
+        PdfReportRow row;
+        for(int i =0; i < 5; i++) {
+        	String indexString = String.valueOf(i + 1);
+        	row = new PdfReportRow();
+        	row.setAmount("10.2");
+        	row.setDate("2024-11-10");
+        	row.setReferenceno("54877" + indexString);
+        	row.setStatus("Active");
+        	row.setRemark("Remarks " + indexString);
+        	tableData.add(row);
+        }
+
+		JRDataSource dataSource = new JRBeanCollectionDataSource(tableData);
+		return dataSource;
     }
     
 }
