6354c93073d6e03abb82daf1f74fafc4b4974339
Ah3laly <Ah3laly@gmail.com>
2024-05-12 11:41:40 +0800

    Define globals

--- app/src/main/java/com/neurogine/services/JapserReportService.java
+++ app/src/main/java/com/neurogine/services/JapserReportService.java
@@ -1,5 +1,8 @@
 package com.neurogine.services;
 
 public interface JapserReportService {
-
+	public static final Integer REPORT_BAND_HEIGHT = 20;
+	public static final Integer REPORT_START_X = 0;
+	public final static Integer REPORT_FIELD_HEIGHTH = 20;
+	public final static Integer REPORT_FIELD_WIDTH = 100;
 }
