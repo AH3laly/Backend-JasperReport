efa97eaa92586f3aa86d5b19c00905de1f312433
Ah3laly <Ah3laly@gmail.com>
2024-05-12 11:47:49 +0800

    Create service implementation plan

--- app/src/main/java/com/neurogine/services/JapserReportServiceImpl.java
+++ app/src/main/java/com/neurogine/services/JapserReportServiceImpl.java
@@ -1,5 +1,28 @@
 package com.neurogine.services;
 
-public class JapserReportServiceImpl implements JapserReportService {
+import com.neurogine.models.ReportDocument;
 
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.design.JRDesignStyle;
+import net.sf.jasperreports.engine.design.JasperDesign;
+
+public class JapserReportServiceImpl implements JapserReportService {
+	
+	public JasperDesign jasperDesign;
+	public JRDesignStyle jasperDesignStyle;
+	public ReportDocument reportDocument;
+	public JRDataSource dataSource;
+	
+	// Plan
+	// Create New Report
+	// define Report Design
+	// Define Report Styles
+	// Build Report
+	// Define Report Fields
+	// Create Document Field
+	// Style Report Field
+	// Build Header Band
+	// Build Body Band
+	// Generate JRXML
+	// Export PDF
 }
