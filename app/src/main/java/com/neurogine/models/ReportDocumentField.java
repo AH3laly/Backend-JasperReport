f34810c6aec0324f53cc9fd7ad7e46b3f88fc951
Ah3laly <Ah3laly@gmail.com>
2024-05-12 11:22:50 +0800

    Create ReportDocumentField model

--- app/src/main/java/com/neurogine/models/ReportDocumentField.java
+++ app/src/main/java/com/neurogine/models/ReportDocumentField.java
@@ -1,5 +1,37 @@
 package com.neurogine.models;
 
 public class ReportDocumentField {
-	
+	private String name;
+	private String type;
+	private String label;
+
+	public ReportDocumentField(String name, String type, String label) {
+		this.name = name;
+		this.type = type;
+		this.label = label;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
 }
