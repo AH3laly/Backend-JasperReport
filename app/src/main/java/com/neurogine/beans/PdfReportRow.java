c3f26e8eca5e26b55bb0c735b9d1dab17c719ad9
Ah3laly <Ah3laly@gmail.com>
2024-05-12 11:22:19 +0800

    Create PdfReportRow bean

--- app/src/main/java/com/neurogine/beans/PdfReportRow.java
+++ app/src/main/java/com/neurogine/beans/PdfReportRow.java
@@ -2,5 +2,47 @@ package com.neurogine.beans;
 
 public class PdfReportRow {
 
+	private Integer id;
+	private String date;
+	private String referenceno;
+	private String amount;
+	private String status;
+	private String remark;
 	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getReferenceno() {
+		return this.referenceno;
+	}
+	public void setReferenceno(String referenceno) {
+		this.referenceno = referenceno;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
 }
