e46816363337322eabaa68f8fbc355306df2db47
Ah3laly <Ah3laly@gmail.com>
2024-05-12 13:30:54 +0800

    Implement JapserReportService service

--- app/src/main/java/com/neurogine/services/JapserReportServiceImpl.java
+++ app/src/main/java/com/neurogine/services/JapserReportServiceImpl.java
@@ -1,9 +1,20 @@
 package com.neurogine.services;
 
+import java.awt.Color;
+
 import com.neurogine.models.ReportDocument;
+import com.neurogine.models.ReportDocumentField;
+
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRLineBox;
+import net.sf.jasperreports.engine.design.JRDesignBand;
+import net.sf.jasperreports.engine.design.JRDesignExpression;
+import net.sf.jasperreports.engine.design.JRDesignField;
+import net.sf.jasperreports.engine.design.JRDesignSection;
+import net.sf.jasperreports.engine.design.JRDesignStaticText;
 import net.sf.jasperreports.engine.design.JRDesignStyle;
+import net.sf.jasperreports.engine.design.JRDesignTextField;
 import net.sf.jasperreports.engine.design.JasperDesign;
 
 public class JapserReportServiceImpl implements JapserReportService {
@@ -26,39 +37,125 @@ public class JapserReportServiceImpl implements JapserReportService {
 	// Generate JRXML
 	// Export PDF
 	
+	public void createNewReport(ReportDocument reportDocument) throws JRException {
+		createNewReport(reportDocument, null);
+	}
+	
 	public void createNewReport(ReportDocument reportDocument, JRDataSource dataSource) throws JRException {
+
+		this.reportDocument = reportDocument;
+		this.dataSource = dataSource;
 		
+		defineReportDesign();
+		
+		// Starting point to build the whole report
+		buildReport();
 	}
 	
 	private void defineReportDesign() throws JRException {
-		
+		jasperDesign = new JasperDesign();
+		jasperDesign.setName(reportDocument.getName());
+		defineReportStyles();
 	}
 	
 	private void defineReportStyles() throws JRException {
-		
+		jasperDesignStyle = new JRDesignStyle();
+		jasperDesignStyle.setName("tableFonts");
+		jasperDesignStyle.setFontSize(8);
+		jasperDesign.addStyle(jasperDesignStyle);
 	}
-	
+
 	private void buildReport() throws JRException {
-		
+		defineReportFields();
+		buildHeaderBand();
+		buildBodyBand();
 	}
 	
 	private void defineReportFields() throws JRException {
-        
+		for(ReportDocumentField reportField : reportDocument.getHeaderFields()) {
+        	jasperDesign.addField(createDocumentField(reportField));
+        }
+	}
+	
+	private JRDesignField createDocumentField(ReportDocumentField reportField) {
+        JRDesignField field = new JRDesignField();
+    	field.setName(reportField.getName());
+    	field.setValueClassName(reportField.getType());
+    	return field;
 	}
 	
 	private void buildHeaderBand() throws JRException {
 		
-	}
-	
-	private void buildBodyBand() {
-
-	}
-
-	public void generateJrxml(String destFileName) throws JRException {
-		
-	}
-	
-	public void exportPdf(String filePath) throws JRException {
+		// Define Header Band
+		JRDesignBand header = new JRDesignBand();
+        header.setHeight(REPORT_BAND_HEIGHT);
         
+		Integer x = REPORT_START_X;
+		for(ReportDocumentField reportField : reportDocument.getHeaderFields()) {
+			JRDesignStaticText tableHeader = new JRDesignStaticText();
+			
+			// Set Content
+			tableHeader.setText(reportField.getLabel());
+			
+			// Set Position Details
+			tableHeader.setHeight(REPORT_FIELD_HEIGHTH);
+			tableHeader.setWidth(REPORT_FIELD_WIDTH);
+            tableHeader.setX(x);
+            tableHeader.setY(0);
+
+            // Set Styles
+            styleReportField(tableHeader.getLineBox());
+            tableHeader.setStyle(jasperDesignStyle);
+            
+            // Push element to Header band
+            header.addElement(tableHeader);
+            
+            x += REPORT_FIELD_WIDTH;
+        }
+		
+		// Inject Table Header to Jasper Design
+		jasperDesign.setPageHeader(header);
 	}
+
+	private void buildBodyBand() {
+	
+		// Define Body Band
+		JRDesignBand body = new JRDesignBand();
+		body.setHeight(REPORT_BAND_HEIGHT);
+	    
+	    Integer x = REPORT_START_X;
+		for(ReportDocumentField reportField : reportDocument.getHeaderFields()) {
+			JRDesignTextField tableRow = new JRDesignTextField();
+			
+			// Set Content
+	        JRDesignExpression colspanExpression = new JRDesignExpression();
+	        colspanExpression.setText("$F{" + reportField.getName() + "}");
+	        tableRow.setExpression(colspanExpression);
+	        
+			// Set Position Details
+			tableRow.setHeight(REPORT_FIELD_HEIGHTH);
+			tableRow.setWidth(REPORT_FIELD_WIDTH);
+			tableRow.setX(x);
+			tableRow.setY(0);
+	        
+			// Set Styles
+	        styleReportField(tableRow.getLineBox());
+	        tableRow.setStyle(jasperDesignStyle);
+	        
+	        body.addElement(tableRow);
+	        
+	        x += REPORT_FIELD_WIDTH;
+		}
+		
+		// Inject Table Header to Jasper Design
+	    ((JRDesignSection)jasperDesign.getDetailSection()).addBand(body);
+	
+	}
+
+	private void styleReportField(JRLineBox lineBox) {
+        lineBox.getPen().setLineWidth(Float.MIN_NORMAL);
+        lineBox.getPen().setLineColor(Color.BLACK);
+        lineBox.setPadding(5);
+	}
+
 }
