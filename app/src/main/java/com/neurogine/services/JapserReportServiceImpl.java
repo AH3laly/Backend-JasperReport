554fcba65545928b189b0d2b48f4846f11a6d78d
Ah3laly <Ah3laly@gmail.com>
2024-05-12 11:53:43 +0800

    Create JapserReportServiceImpl methods declaration

--- app/src/main/java/com/neurogine/services/JapserReportServiceImpl.java
+++ app/src/main/java/com/neurogine/services/JapserReportServiceImpl.java
@@ -1,8 +1,8 @@
 package com.neurogine.services;
 
 import com.neurogine.models.ReportDocument;
-
 import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.design.JRDesignStyle;
 import net.sf.jasperreports.engine.design.JasperDesign;
 
@@ -25,4 +25,40 @@ public class JapserReportServiceImpl implements JapserReportService {
 	// Build Body Band
 	// Generate JRXML
 	// Export PDF
+	
+	public void createNewReport(ReportDocument reportDocument, JRDataSource dataSource) throws JRException {
+		
+	}
+	
+	private void defineReportDesign() throws JRException {
+		
+	}
+	
+	private void defineReportStyles() throws JRException {
+		
+	}
+	
+	private void buildReport() throws JRException {
+		
+	}
+	
+	private void defineReportFields() throws JRException {
+        
+	}
+	
+	private void buildHeaderBand() throws JRException {
+		
+	}
+	
+	private void buildBodyBand() {
+
+	}
+
+	public void generateJrxml(String destFileName) throws JRException {
+		
+	}
+	
+	public void exportPdf(String filePath) throws JRException {
+        
+	}
 }
