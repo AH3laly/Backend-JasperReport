4c5491fa7c935ba874fc0c26534ffb5a43dd1d3e
Ah3laly <Ah3laly@gmail.com>
2024-05-12 11:23:33 +0800

    Create ReportDocument model

--- app/src/main/java/com/neurogine/models/ReportDocument.java
+++ app/src/main/java/com/neurogine/models/ReportDocument.java
@@ -1,5 +1,21 @@
 package com.neurogine.models;
 
-public class ReportDocument {
+import java.util.List;
 
+public class ReportDocument {
+	private String name;
+	private List<ReportDocumentField> headerFields;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<ReportDocumentField> getHeaderFields() {
+		return headerFields;
+	}
+	public void setHeaderFields(List<ReportDocumentField> headerFields) {
+		this.headerFields = headerFields;
+	}
 }
